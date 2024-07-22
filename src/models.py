c827d0832509ee80f27d92db2f8169105e7894d3
Joachim Bosch <164736830+JoachimBosch@users.noreply.github.com>
2024-07-22 19:02:19 +0000

    First commit

--- src/models.py
+++ src/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -17,3 +17,12 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    rotation_period = db.Column(db.Integer)
+    orbital_period = db.Column(db.Integer)
+    diameter = db.Column(db.Integer)
+    climate = db.Column(db.String(250))
+    terrain = db.Column(db.String(250))
