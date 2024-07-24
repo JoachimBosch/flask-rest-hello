8fd2015f02c4328c8faeaeb6971ee64de1d58dcd
Joachim Bosch <164736830+JoachimBosch@users.noreply.github.com>
2024-07-24 12:49:07 +0000

    Finalized models basics and started working on routes

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Person, Planet, Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,31 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/people', methods=['GET'])
+def get_all_people():
+    people = Person.query.all()
+    return jsonify(people), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/people/<int:person_id>', methods=['GET'])
+def get_person(person_id):
+    person = Person.query.get(person_id)
+    if person:
+        return jsonify(person), 200
+    else:
+        return "Person not found", 404
+    
+@app.route('/planets', methods=['GET'])
+def get_all_planets():
+    planets = Planet.query.all()
+    return jsonify(planets), 200
 
-    return jsonify(response_body), 200
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+    if planet:
+        return jsonify(planet), 200
+    else:
+        return "Planet obliviated", 404
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -6,7 +6,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -26,7 +25,6 @@ class Person(db.Model):
         return {
             "id": self.id,
             "name": self.name,
-            # do not serialize the password, its a security breach
         }
 
 
@@ -39,17 +37,18 @@ class Planet(db.Model):
         return {
             "id": self.id,
             "name": self.name,
-            # do not serialize the password, its a security breach
         }
 
 class Favorites(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     fav_planets = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    fav_people = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=True)
 
     def serialize(self):
         return {
             "id": self.id,
             "user_id": self.user_id,
-            "favorites": self.fav_planets,
+            "fav_planets": self.fav_planets,
+            "fav_people": self.fav_people,
         }
